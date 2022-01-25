fa9f53bb61325b76c6a4885ba3f0a847c61a79d1
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-01-26 06:17:58 +0700

    backup

--- src/ReduxThunk/route/routes.js
+++
@@ -1,5 +0,0 @@
-import Akun from '../screens/bottom-tabs/Akun';
-import Keranjang from '../screens/bottom-tabs/Keranjang';
-import Dashboard from '../screens/Dashboard';
-
-export {Dashboard, Keranjang, Akun};
