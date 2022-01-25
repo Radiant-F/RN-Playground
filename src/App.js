5686a99647f0c22e9d470e773a355743597acc0f
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-01-26 06:18:20 +0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-// import React from 'react';
-// import Navigator from './MessAround/route/Navigator';
-// import {store} from './MessAround/redux/Store';
-// import {Provider} from 'react-redux';
-
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       <Navigator />
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
-// Redux Thunk
 import React from 'react';
-import Navigator from './ReduxThunk/route/Navigator';
+import Navigator from './MessAround/route/Navigator';
+import {store} from './MessAround/redux/Store';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './ReduxThunk/redux/reducers';
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 function App() {
   return (
