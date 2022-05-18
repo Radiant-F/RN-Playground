3c9a9e13352cee885834e3e838a0e5750520e3e6
Radiant-F <radiantfadilah0@gmail.com>
2022-05-18 10:08:34 +0700

    rename url

--- src/ReduxToolkit/utils/APIs.js
+++ src/ReduxToolkit/utils/APIs.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const url = 'http://restful-api-laravel-7.herokuapp.com/api';
-const config = token => ({
+const url = 'https://memories-saya.herokuapp.com';
+const urlConfig = token => ({
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
 
-export const fetchLogin = formData => axios.post(`${url}/login`, formData);
-export const fetchUserDetail = token => axios.get(`${url}/user`, config(token));
-export const fetchTodo = token => axios.get(`${url}/todo`, config(token));
+export const fetchLogin = formData =>
+  axios.post(`${url}/user/signin`, formData);
+export const fetchUserDetail = token =>
+  axios.get(`${url}/user`, urlConfig(token));
+export const fetchTodo = token => axios.get(`${url}/todo`, urlConfig(token));
 export const fetchPokemon = name => axios.get(`${url}/${name}`);
