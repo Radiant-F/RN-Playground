c558e37c448fb0dbc65768bacb91441a208947c0
Radiant-F <radiantfadilah0@gmail.com>
2022-05-18 10:08:46 +0700

    Add: extra reducers

--- src/ReduxToolkit/redux/reducers/auth.js
+++ src/ReduxToolkit/redux/reducers/auth.js
@@ -1,10 +1,12 @@
 import {createSlice} from '@reduxjs/toolkit';
+import {login} from '../actions/auth';
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
     auth_type: 'login',
     user_data: null,
+    status: null,
   },
   reducers: {
     authType: (state, action) => {
@@ -12,6 +14,17 @@ export const authSlice = createSlice({
     },
     userData: (state, action) => {
       state.user_data = action.payload;
+    },
+  },
+  extraReducers: {
+    [login.pending]: state => {
+      state.status = 'loading';
+    },
+    [login.fulfilled]: state => {
+      state.status = 'success';
+    },
+    [login.rejected]: state => {
+      state.status = 'error';
     },
   },
 });
