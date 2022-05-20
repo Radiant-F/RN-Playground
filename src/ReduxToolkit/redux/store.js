f62546cddf4dc2f3906be4e2d99264a72802f09a
Radiant-F <radiantfadilah0@gmail.com>
2022-05-20 08:00:38 +0700

    Add: posts reducer

--- src/ReduxToolkit/redux/store.js
+++ src/ReduxToolkit/redux/store.js
@@ -3,7 +3,8 @@ import counter from './reducers/counter';
 import auth from './reducers/auth';
 import fetching from './reducers/fetching';
 import todo from './reducers/todo';
+import posts from './reducers/posts';
 
 export const store = configureStore({
-  reducer: {counter, auth, fetching, todo},
+  reducer: {counter, auth, fetching, todo, posts},
 });
