a3e4507df81c1ddbfde12a4ba8c015777653c66c
Radiant-F <radiantfadilah0@gmail.com>
2022-05-20 13:31:47 +0700

    Add: get posts request

--- src/ReduxToolkit/screens/Dashboard/Dashboard.js
+++ src/ReduxToolkit/screens/Dashboard/Dashboard.js
@@ -2,23 +2,29 @@ import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {getTodo} from '../../redux/actions/todo';
+import {getPosts} from '../../redux/actions/posts';
 
 export default function Dashboard() {
   const dispatch = useDispatch();
-  const {todo, auth} = useSelector(state => state);
+  const {data, status} = useSelector(state => state.posts.posts);
 
-  // useEffect(() => {
-  //   dispatch(getTodo(auth.user_data.token));
-  // }, [dispatch]);
+  useEffect(() => {
+    console.log(data);
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <View>
       <Text>Dashboard</Text>
-      {todo.todos_list?.map((value, index) => (
-        <Text key={index} style={{color: 'black'}}>
-          {value.task}
-        </Text>
-      ))}
+      {status == 'loading' && <ActivityIndicator />}
+      {data?.length == 0 && <Text>Kosong</Text>}
+      {data &&
+        data.length > 0 &&
+        data.map((value, index) => (
+          <Text key={index} style={{color: 'black'}}>
+            {value.title}
+          </Text>
+        ))}
     </View>
   );
 }
