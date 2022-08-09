7d0d59af4cfde786e9dba493c25321a1e59431e7
Radiant-F <radiantfadilah0@gmail.com>
2022-08-09 14:11:13 +0700

    new screens

--- src/@Template/screens/index.js
+++ src/@Template/screens/index.js
@@ -1,5 +1,10 @@
 import Auth from './Auth/Auth';
-import Dashboard from './Dashboard/Dashboard';
+import Profile from './BottomTabs/Profile/Profile';
+import Dashboard from './BottomTabs/Dashboard/Dashboard';
 import DrawerContent from './Drawer/DrawerContent';
+import Media from './BottomTabs/Media/Media';
+import Misc from './BottomTabs/Misc/Misc';
+import Settings from './Settings/Settings';
+import Videos from './BottomTabs/Media/Videos';
 
-export {Auth, Dashboard, DrawerContent};
+export {Auth, Dashboard, DrawerContent, Profile, Media, Misc, Settings, Videos};
