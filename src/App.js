bc1baa91770c33ee735efcc38da9fb60c9c3ac75
Radiant-F <radiantfadilah0@gmail.com>
2022-09-02 14:26:12 +0700

    excercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 // ========= Template =========
-import React from 'react';
-import {
-  Navigator,
-  BottomNavigator,
-  DrawerNavigator,
-} from './@Template/routes/Navigator';
-import {Provider} from 'react-redux';
-import {store} from './@Template/redux/store';
-import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+// import React from 'react';
+// import {
+//   Navigator,
+//   BottomNavigator,
+//   DrawerNavigator,
+// } from './@Template/routes/Navigator';
+// import {Provider} from 'react-redux';
+// import {store} from './@Template/redux/store';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <DrawerNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </Provider>
-  );
-}
+// export default function App() {
+//   return (
+//     <Provider store={store}>
+//       <SafeAreaProvider>
+//         <NavigationContainer>
+//           <DrawerNavigator />
+//         </NavigationContainer>
+//       </SafeAreaProvider>
+//     </Provider>
+//   );
+// }
 
 // ========= Firebase =========
 // import React from 'react';
@@ -37,18 +37,24 @@ export default function App() {
 // }
 
 // ========= Excercise =========
-// import React from 'react';
-// import Navigator from './Excercise/routes/Navigator';
-// import {store} from './Excercise/redux/store';
-// import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {StackScreen} from './Excercise/routes/Navigator';
+import {Provider} from 'react-redux';
+import {store} from './Excercise/redux/store';
 
-// export default function App() {
-//   return (
-//     <Provider store={store}>
-//       <Navigator />
-//     </Provider>
-//   );
-// }
+export default function App() {
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StackScreen />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
+  );
+}
 
 // ========= Firebase =========
 // import React from 'react';
