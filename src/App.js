3cc3bdf86da6da8bb6b11a41b3685a4f1f820a68
Radiant-F <radiantfadilah0@gmail.com>
2022-09-10 16:36:29 +0700

    update category

--- src/App.js
+++ src/App.js
@@ -37,24 +37,24 @@
 // }
 
 // ========= Excercise =========
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {StackScreen} from './Excercise/routes/Navigator';
-import {Provider} from 'react-redux';
-import {store} from './Excercise/redux/store';
+// import {NavigationContainer} from '@react-navigation/native';
+// import React from 'react';
+// import {SafeAreaProvider} from 'react-native-safe-area-context';
+// import {StackScreen} from './Excercise/routes/Navigator';
+// import {Provider} from 'react-redux';
+// import {store} from './Excercise/redux/store';
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <StackScreen />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </Provider>
-  );
-}
+// export default function App() {
+//   return (
+//     <Provider store={store}>
+//       <SafeAreaProvider>
+//         <NavigationContainer>
+//           <StackScreen />
+//         </NavigationContainer>
+//       </SafeAreaProvider>
+//     </Provider>
+//   );
+// }
 
 // ========= Firebase =========
 // import React from 'react';
@@ -70,6 +70,23 @@ export default function App() {
 //   );
 // }
 
+// ========= Libraries =========
+import React from 'react';
+import Navigator from './Libraries/routes/Navigator';
+import {Provider} from 'react-redux';
+import {store} from './Libraries/redux/store';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Navigator />
+      </SafeAreaProvider>
+    </Provider>
+  );
+}
+
 // ========= RTK Query =========
 // import React from 'react';
 // import Navigator from './RTKQuery/routes/Navigator';
@@ -81,5 +98,50 @@ export default function App() {
 //     <Provider store={store}>
 //       <Navigator />
 //     </Provider>
+//   );
+// }
+
+// ========= React Native Bootsplash =========
+// import React, {useEffect} from 'react';
+// import {
+//   Navigator,
+//   BottomNavigator,
+//   DrawerNavigator,
+// } from './Bootsplash/routes/Navigator';
+// import {Provider} from 'react-redux';
+// import {store} from './Bootsplash/redux/store';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {SafeAreaProvider} from 'react-native-safe-area-context';
+// import {Text, View} from 'react-native';
+// import RNBootSplash from 'react-native-bootsplash';
+
+// export default function App() {
+//   return (
+//     <Provider store={store}>
+//       <SafeAreaProvider>
+//         <NavigationContainer onReady={() => RNBootSplash.hide({fade: true})}>
+//           <DrawerNavigator />
+//         </NavigationContainer>
+//       </SafeAreaProvider>
+//     </Provider>
+//   );
+// }
+
+// export default function App() {
+//   useEffect(() => {
+//     const init = async () => {
+//       console.log('the app is running 😲');
+//       // …do multiple sync or async tasks
+//     };
+
+//     init().finally(async () => {
+//       await RNBootSplash.hide({fade: true});
+//       console.log('Bootsplash has been hidden successfully');
+//     });
+//   }, []);
+//   return (
+//     <View>
+//       <Text>ping?</Text>
+//     </View>
 //   );
 // }
